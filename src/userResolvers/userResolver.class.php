4bb40f4a70755b6129b3b67dc03a734c6743212f
Alban de La Bretèche <a.delabreteche@criteo.com>
2015-07-07 16:02:17 +0200

    Clean match

--- src/userResolvers/userResolver.class.php
+++ src/userResolvers/userResolver.class.php
@@ -1,27 +1,9 @@
 <?php
 class UserResolver
 {
-    var $partner = '42';
-
-    function getUserId($cookies) {
-
-        if (isset($cookies['uid'])) {
-            return $cookies['uid'];
-        }
-
-        if (!isset($cookies['Ids']))
-        {
-            return '';
-        }
-
-        $ids = @unserialize($cookies['Ids']);
-
-        if ($ids == null || !is_array($ids) || !array_key_exists($this->partner, $ids))
-        {
-            return '';
-        }
-
-        return $ids[$this->partner];
+    function getUserId($cookies)
+    {
+        return isset($cookies['uid']) ? $cookies['uid'] : '';
     }
 }
 ?>
