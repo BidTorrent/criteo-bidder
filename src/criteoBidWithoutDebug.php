a2eeed177296f9d75e4a315906c6be31b9719b9f
Alban de La Bretèche <a.delabreteche@criteo.com>
2015-07-08 18:59:13 +0200

    Add Bidder without debug

---
+++ src/criteoBidWithoutDebug.php
@@ -0,0 +1,25 @@
+<?php
+
+    error_reporting(E_ALL);
+    ini_set('display_errors', 'on');
+
+    include('decoders/decoder.class.php');
+    include('bidders/criteoBidder.class.php');
+    include('bidders/wrapperBidder.class.php');
+
+    $decoder = new Decoder('keys/key-1-private.pem');
+    $innerBidder = new CriteoBidder('http://rtb-validation.fr.eu.criteo.com/delivery/auction/request?profile=61');
+
+    $bidder = new WrapperBidder(
+        $decoder,
+        $innerBidder
+    );
+
+    $request = json_decode(file_get_contents("php://input"), true);
+    $response = $bidder->GetResponse($request);
+    header('Content-Type: application/json');
+    header('Cache-Control: no-cache, no-store, must-revalidate');
+    header('Pragma: no-cache');
+    header('Expires: 0');
+    echo json_encode($response);
+?>
